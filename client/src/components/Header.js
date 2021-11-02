fb7a8b3c38c95e2919a53ae17c48b9dba8f97150
Jacob White <jacobewhitedev@gmail.com>
2021-11-02 00:35:39 -0500

    homepage svg changed, social icons added

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
     <div id="HeaderContainer">
-      <header className="px-24 py-5 max-w-7xl mx-auto">
+      <header className="px-12 py-5 max-w-7xl mx-auto">
         <p className="uppercase font-jSans text-2xl">ga</p>
         <nav>
           <a href="/"></a>
