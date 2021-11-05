6f8cc3bef4a424e2f6e2232d78f3cddadf3d55db
Jacob White <jacobewhitedev@gmail.com>
2021-11-04 21:52:53 -0500

    configured server the mongo way, get jpg collection functioning

--- server/server.js
+++ server/server.js
@@ -1,13 +1,18 @@
-import express from "express";
-import cors from "cors";
-import artCollection from "./api/artCollection.route.js";
-
+const express = require("express");
 const app = express();
-
+const cors = require("cors");
+require("dotenv").config({ path: "./config.env" });
+const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
+app.use(require("./routes/record"));
+// get driver connection
+const dbo = require("./db/conn");
 
-app.use("/api/v1/art-collection", artCollection);
-app.use("*", (req, res) => res.status(404).json({ error: "not found bud" }));
-
-export default app;
+app.listen(port, () => {
+  // perform a database connection when server starts
+  dbo.connectToServer(function (err) {
+    if (err) console.error(err);
+  });
+  console.log(`Server is running on port: ${port}`);
+});
