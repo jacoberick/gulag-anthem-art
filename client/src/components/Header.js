26abc226766523ba9f4758e63d7b712099969543
Jacob White <jacobewhitedev@gmail.com>
2021-11-09 21:58:42 -0600

    hamburger button added and conditionally rendered

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,35 +1,44 @@
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+
+const linkClass =
+  "uppercase text-base hover:text-trainBlue transition bOne:text-sm ";
+
+const desktopNav = (
+  <nav className="font-zen bFour:hidden">
+    <a className={linkClass} href="/#artContainer">
+      art
+    </a>
+    <a
+      rel="noreferrer"
+      className={`${linkClass} mx-12`}
+      href="https://www.etsy.com/shop/GulagAnthem"
+      target="_blank"
+    >
+      shop
+    </a>
+    <Link className={linkClass} to="/contact">
+      contact
+    </Link>
+  </nav>
+);
+
+const mobileNav = <nav className=""></nav>;
 
 const Header = () => {
-  const { pathname } = useLocation();
-
-  if (pathname === "/treelink") return null;
-
-  const linkClass =
-    "uppercase text-base hover:text-trainBlue transition bOne:text-sm ";
   return (
     <div id="HeaderContainer">
-      <header className="px-14 py-3 max-w-screen-2xl mx-auto flex items-center justify-between">
+      <header className="px-14 bFour:px-7 py-3 max-w-screen-2xl mx-auto flex items-center justify-between">
         <Link to="/">
           <p className="uppercase font-jSans text-2xl">ga</p>
         </Link>
-        <nav className="font-zen">
-          <a className={linkClass} href="/#artContainer">
-            art
-          </a>
-          <a
-            rel="noreferrer"
-            className={`${linkClass} mx-12`}
-            href="https://www.etsy.com/shop/GulagAnthem"
-            target="_blank"
-          >
-            shop
-          </a>
-          <Link className={linkClass} to="/contact">
-            contact
-          </Link>
-        </nav>
+        {desktopNav}
+        <button className="hidden bFour:block">
+          <FontAwesomeIcon className="text-3xl text-black" icon={faBars} />
+        </button>
       </header>
+      {mobileNav}
     </div>
   );
 };
