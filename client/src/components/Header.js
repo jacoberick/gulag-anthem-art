2f9fa8ffd459c331f93e9fcfc1fbe627a022be5d
Jacob White <jacobewhitedev@gmail.com>
2021-11-09 23:32:12 -0600

    mobile nav completed

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,12 +1,15 @@
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 const linkClass =
-  "uppercase text-base hover:text-trainBlue transition bOne:text-sm ";
+  "uppercase text-base hover:text-trainBlue transition bOne:text-sm font-zen";
+
+const mobileLinkClass = "uppercase font-zen text-3xl";
 
 const desktopNav = (
-  <nav className="font-zen bFour:hidden">
+  <nav className="bFour:hidden">
     <a className={linkClass} href="/#artContainer">
       art
     </a>
@@ -24,9 +27,52 @@ const desktopNav = (
   </nav>
 );
 
-const mobileNav = <nav className=""></nav>;
-
 const Header = () => {
+  const [activeBurger, setActiveBurger] = useState(false);
+
+  const handleBurgerClick = () => {
+    document.getElementById("burgerButton").classList.toggle("rotate-90");
+    document
+      .getElementsByTagName("body")[0]
+      .classList.toggle("overflow-hidden");
+    setActiveBurger(!activeBurger);
+  };
+
+  const mobileNav = (
+    <div
+      id="mobileNavContainer"
+      className={`transition-all hidden bFour:block bg-trainBlue text-white h-screenMinusHeader w-3/4 absolute ${
+        activeBurger ? "right-0" : "right-full"
+      } z-10 p-5`}
+    >
+      <nav className="flex flex-col justify-center h-full pb-32">
+        <a
+          onClick={handleBurgerClick}
+          className={mobileLinkClass}
+          href="/#artContainer"
+        >
+          art
+        </a>
+        <a
+          onClick={handleBurgerClick}
+          rel="noreferrer"
+          className={`${mobileLinkClass} py-8`}
+          href="https://www.etsy.com/shop/GulagAnthem"
+          target="_blank"
+        >
+          shop
+        </a>
+        <Link
+          onClick={handleBurgerClick}
+          className={mobileLinkClass}
+          to="/contact"
+        >
+          contact
+        </Link>
+      </nav>
+    </div>
+  );
+
   return (
     <div id="HeaderContainer">
       <header className="px-14 bFour:px-7 py-3 max-w-screen-2xl mx-auto flex items-center justify-between">
@@ -34,7 +80,11 @@ const Header = () => {
           <p className="uppercase font-jSans text-2xl">ga</p>
         </Link>
         {desktopNav}
-        <button className="hidden bFour:block">
+        <button
+          id="burgerButton"
+          onClick={handleBurgerClick}
+          className="hidden bFour:block transform transition"
+        >
           <FontAwesomeIcon className="text-3xl text-black" icon={faBars} />
         </button>
       </header>
