419b2782ce5ba045d6a5571a461ccac2676c6d19
Jacob White <jacobewhitedev@gmail.com>
2021-12-21 01:00:26 -0600

    x translation working

--- client/src/components/MadeWithCode.js
+++ client/src/components/MadeWithCode.js
@@ -10,17 +10,24 @@ const MadeWithCode = () => {
 
   const followMouse = (e) => {
     let container = e.currentTarget;
+    let img = container.querySelector("img");
     let text = container.querySelector("p");
-    let x = container.offsetWidth / 2 - e.nativeEvent.offsetX;
-    let y = container.offsetHeight / 2 - e.nativeEvent.offsetY / 15;
-    text.style.transform = `translate(${x}px, ${y}px)`;
-    console.log(container.offsetHeight, e.nativeEvent);
+
+    let textX =
+      (container.offsetWidth / 2 - e.nativeEvent.offsetX) / 15 -
+      text.offsetWidth / 2;
+    let imgX = (textX + text.offsetWidth / 2) / 10;
+
+    let y = e.nativeEvent.offsetY / 5;
+    text.style.transform = `translate(${textX}px, -50%)`;
+    img.style.transform = `translate(${imgX}px)`;
   };
 
   const returnTextPosition = (e) => {
     let text = e.currentTarget.querySelector("p");
-    text.innerHTML = "UMBRELLA";
+    let img = e.currentTarget.querySelector("img");
     text.style.transform = "translate(-50%, -50%)";
+    img.style.transform = "translate(0)";
   };
 
   return (
@@ -30,22 +37,27 @@ const MadeWithCode = () => {
           generative projects
         </h1>
         <hr className="border-t-2 border-black" />
-        <div id="projectsContainer" className="">
+        <div
+          id="projectsContainer"
+          className=""
+          onMouseMove={followMouse}
+          onMouseLeave={returnTextPosition}
+        >
           {genProjectsInfo.map((x) => (
-            <a
-              href={x.href}
-              target="_blank"
-              rel="noreferrer"
-              className="h-52 overflow-hidden flex items-center my-10 relative transition duration-200 transform"
-              key={x.id}
-              onMouseMove={followMouse}
-              onMouseLeave={returnTextPosition}
-            >
-              <img src={`./img/${x.imgSrc}`} alt={x.name} className="" />
-              <p className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white font-zen text-5xl transition-all duration-75 whitespace-nowrap pointer-events-none">
-                {x.name}
-              </p>
-            </a>
+            <div className="my-10">
+              <a
+                href={x.href}
+                target="_blank"
+                rel="noreferrer"
+                className="h-52 overflow-hidden flex items-center relative transition duration-200 transform"
+                key={x.id}
+              >
+                <img src={`./img/${x.imgSrc}`} alt={x.name} />
+                <p className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white font-zen text-5xl transition-all duration-75 whitespace-nowrap pointer-events-none">
+                  {x.name}
+                </p>
+              </a>
+            </div>
           ))}
         </div>
       </div>
