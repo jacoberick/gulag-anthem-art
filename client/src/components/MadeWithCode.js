68b51819afa276da3dc0b8d973fe81186c73aa81
Jacob White <jacobewhitedev@gmail.com>
2021-12-22 16:45:20 -0600

    y cords functionality working for parallax

--- client/src/components/MadeWithCode.js
+++ client/src/components/MadeWithCode.js
@@ -8,26 +8,34 @@ const MadeWithCode = () => {
     },
   ];
 
-  const followMouse = (e) => {
+  const parallaxMe = (e) => {
     let container = e.currentTarget;
+    let containerRect = container.getBoundingClientRect();
+    let mouseX = e.clientX - containerRect.left;
+    let mouseY = e.clientY - containerRect.top;
     let img = container.querySelector("img");
     let text = container.querySelector("p");
 
+    // x values
     let textX =
-      (container.offsetWidth / 2 - e.nativeEvent.offsetX) / 15 -
-      text.offsetWidth / 2;
+      (container.offsetWidth / 2 - mouseX) / 15 - text.offsetWidth / 2;
     let imgX = (textX + text.offsetWidth / 2) / 10;
 
-    let y = e.nativeEvent.offsetY / 5;
-    text.style.transform = `translate(${textX}px, -50%)`;
-    img.style.transform = `translate(${imgX}px) scale(1.5)`;
+    // y values
+    let textY =
+      (container.offsetHeight / 2 - mouseY) / 15 - text.offsetHeight / 2;
+    let imgY = textY / 10;
+
+    //style updates
+    text.style.transform = `translate(${textX}px, ${textY}px)`;
+    img.style.transform = `translate(${imgX}px, ${imgY}px) scale(1.25)`;
   };
 
-  const returnTextPosition = (e) => {
+  const resetParallax = (e) => {
     let text = e.currentTarget.querySelector("p");
     let img = e.currentTarget.querySelector("img");
     text.style.transform = "translate(-50%, -50%)";
-    img.style.transform = "translate(0)";
+    img.style.transform = "translate(0) scale(1.25)";
   };
 
   return (
@@ -37,25 +45,24 @@ const MadeWithCode = () => {
           generative projects
         </h1>
         <hr className="border-t-2 border-black" />
-        <div
-          id="projectsContainer"
-          className=""
-          onMouseMove={followMouse}
-          onMouseLeave={returnTextPosition}
-        >
+        <div id="projectsContainer" className="">
           {genProjectsInfo.map((x) => (
-            <div className="my-10">
+            <div
+              onMouseMove={parallaxMe}
+              onMouseLeave={resetParallax}
+              key={x.id}
+              className="my-10 linkContainer"
+            >
               <a
                 href={x.href}
                 target="_blank"
                 rel="noreferrer"
                 className="h-52 overflow-hidden flex items-center relative transform"
-                key={x.id}
               >
                 <img
                   src={`./img/${x.imgSrc}`}
                   alt={x.name}
-                  className="transform scale-150"
+                  className="transform scale-125"
                 />
                 <p className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white font-zen text-5xl duration-75 whitespace-nowrap pointer-events-none">
                   {x.name}
